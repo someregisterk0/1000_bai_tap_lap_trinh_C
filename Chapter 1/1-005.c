ca447ea220b655ab304b153eb84387cd3bd365a7
danh nh <someregisterk0@gmail.com>
2020-07-24 10:41:32 +0700

    add all files

---
+++ Chapter 1/1-005.c
@@ -0,0 +1,13 @@
+/*Chương 1 - Bài 5 - trang 4*/
+
+#include <stdio.h>
+
+int main() {
+	float s = 0;
+	int n = 9;
+
+	for(int i = 0; i <= n; ++i) {
+		s += 1.0 / (2 * i + 1);
+	}
+	printf("S = %.2f", s);
+}
---
+++ Chapter 1/1-023.c
@@ -0,0 +1,16 @@
+/*Chương 1 - Bài 23 - trang 6*/
+
+#include <stdio.h>
+
+int main() {
+	int n = 99, count = 0;
+
+    printf("Uoc so cua %d la: ", n);
+    for(int i = 1; i <= n; ++i) {
+        if(n % i == 0) {
+            printf("%4d", i);
+            ++count;
+        }
+    }
+    printf("\ncount = %d", count);
+}
